7229e76d73e6e5059fe9758fa71746152a57ef0a
Mat Whiteside <matwhiteside1@gmail.com>
2017-12-18 21:31:42 +0000

    Added some comments to menu class, removed unused imports.

--- src/Menu.java
+++ src/Menu.java
@@ -3,9 +3,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -21,30 +18,35 @@ public class Menu extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception  {
+        // Create the parent layout and the scene
         VBox root = new VBox();
         Scene s = new Scene(root, 300, 300);
 
+        // Create a title
         Label title = new Label("Menu");
         title.setFont(new Font("Verdana", 20));
 
+        // Create a button for the reaction test
         Button reactionTestButton = new Button("Reaction test");
         reactionTestButton.setPrefSize(150, 40);
         reactionTestButton.setOnAction(event -> {
             primaryStage.setScene(new ReactionTest().createScene());
         });
 
-
+        // Create a button for the trace test
         Button traceTestButton = new Button("Trace test");
         traceTestButton.setPrefSize(150, 40);
         traceTestButton.setOnAction(event -> {
             primaryStage.setScene(new TraceTest().createScene());
         });
 
+        // Center everything in the VBox, set the spacing between elements and add all the elements
+        // to the layout.
         root.alignmentProperty().setValue(Pos.CENTER);
         root.setSpacing(10);
         root.getChildren().addAll(title, reactionTestButton, traceTestButton);
 
-
+        // Set the primary stage to the scene we've made
         primaryStage.setScene(s);
         primaryStage.show();
     }
